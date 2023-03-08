39e321cebd66112230ca651c16a6f3624ec9734c
ashleymckissickjr <127322158+ashleymckissickjr@users.noreply.github.com>
2023-03-08 15:39:15 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BJ_s_Strong_rate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BJ's Power Rates");
+            int a = 1000;
+            int b = 1000;
+            int c = 1000;
+            int d = 1000;
+            int e = a + b + c + d;
+            Console.WriteLine(e);
+            Console.ReadKey();
+        }
+    }
+}
