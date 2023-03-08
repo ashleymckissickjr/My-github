751ec41dd0110d65ff2bdb66fbb1df1dc421ac3c
ashleymckissickjr <127322158+ashleymckissickjr@users.noreply.github.com>
2023-03-08 15:41:04 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,19 +1,39 @@
 ﻿using System;
 
-namespace BJ_s_Strong_rate
+namespace BJ_s_and_His_Own_Characters
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("BJ's Power Rates");
-            int a = 1000;
-            int b = 1000;
-            int c = 1000;
-            int d = 1000;
-            int e = a + b + c + d;
-            Console.WriteLine(e);
-            Console.ReadKey();
+            Console.WriteLine("BJ");
+            Console.WriteLine("Amcassle");
+            Console.WriteLine("Amkissria");
+            Console.WriteLine("Ashleia");
+            Console.WriteLine("Bleeila");
+            Console.WriteLine("Briliya");
+            Console.WriteLine("Croissickleya");
+            Console.WriteLine("Deailea");
+            Console.WriteLine("Lamistra");
+            Console.WriteLine("Saralera");
+            Console.WriteLine("Karileia");
+            Console.WriteLine("Kwanjaleia");
+            Console.WriteLine("Lrasila")
+            Console.WriteLine("Alimokia");
+            Console.WriteLine("Isashleya");
+            Console.WriteLine("Bryanku");
+            Console.WriteLine("Bryantjay");
+            Console.WriteLine("Cromckjay");
+            Console.WriteLine("Gryanick");
+            Console.WriteLine("Cranick");
+            Console.WriteLine("Samciant");
+            Console.WriteLine("Boomjay");
+            Console.WriteLine("Mccastick");
+            Console.WriteLine("Falmorck");
+            Console.WriteLine("Gemarkon");
+            Console.WriteLine("Beaicken");
+            Console.WriteLine("Brayant");
+            Console.WriteLine("Emlimckio");
         }
     }
 }
